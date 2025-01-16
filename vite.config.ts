fb6bef37535bd413753375dbe410af8ab323b1ae
teenage1ove <kirillstars7@gmail.com>
2025-01-16 12:13:48 +0300

    init vite project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-challenge/',
+  // base: '/frontend-challenge/',
   plugins: [react()],
 })
