609b876e1eb16c8839ff609a48de4d86e5eafd5b
teenage1ove <kirillstars7@gmail.com>
2025-01-16 23:47:25 +0300

    delete app component

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-	return <div>Прив</div>
-}
-
-export default App
---
+++ src/Pages/App/App.tsx
@@ -0,0 +1,11 @@
+import { Cats } from '../Cats/Cats'
+
+function App() {
+	return (
+		<div>
+			<Cats />
+		</div>
+	)
+}
+
+export default App
