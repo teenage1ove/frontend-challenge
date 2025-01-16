2ed68fe78b3df0d57edcfe788f614933d07cdb66
teenage1ove <kirillstars7@gmail.com>
2025-01-16 23:48:32 +0300

    add routes and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { FavoritesCats } from './Pages/FavoritesCats/FavoritesCats.tsx'
+import { NotFound } from './Pages/NotFound/NotFound.tsx'
+import { Layout } from './components/Layout/Layout.tsx'
+import { Cats } from './Pages/Cats/Cats.tsx'
+import './index.scss'
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <Cats />,
+				},
+
+				{
+					path: '/favorites',
+					element: <FavoritesCats />,
+				},
+			],
+		},
+		{
+			path: '*',
+			element: <NotFound />,
+		},
+	],
+	{
+		future: { v7_relativeSplatPath: true },
+	}
+)
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<RouterProvider router={router} future={{ v7_startTransition: true }} />
 )
