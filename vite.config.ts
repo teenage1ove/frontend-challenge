85aaa691c9ddf82d448594ca4fb875b7eadaa991
teenage1ove <kirillstars7@gmail.com>
2025-01-17 12:42:57 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/frontend-challenge/', // для ghpages
   plugins: [react()],
   define: {
     'VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
