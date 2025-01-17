206cada62d09df2722fe7a73c93b4f737f81f009
teenage1ove <kirillstars7@gmail.com>
2025-01-17 12:46:17 +0300

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend-challenge/', 
   plugins: [react()],
   define: {
     'VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
